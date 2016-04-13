5b1cb02ff59e07a73b106dc7fd8c58ad7a06e28e
Karine <karine.jamet203@gmail.com>
2016-04-13 19:40:28 +0200

    test

--- script.js
+++ script.js
@@ -21,4 +21,27 @@ $(document).ready(function() {
     $('.intro').addClass("hide");
   });
 
+  function hasGetUserMedia() {
+	return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
+		navigator.mozGetUserMedia || navigator.msGetUserMedia);
+}
+
+var webcamError = function(e) {
+	alert('Webcam error!', e);
+};
+
+var video = $('#webcam')[0];
+
+if (navigator.getUserMedia) {
+	navigator.getUserMedia({audio: true, video: true}, function(stream) {
+		video.src = stream;
+	}, webcamError);
+} else if (navigator.webkitGetUserMedia) {
+         navigator.webkitGetUserMedia({audio:true, video:true}, function(stream) {
+		video.src = window.webkitURL.createObjectURL(stream);
+	}, webcamError);
+} else {
+	//video.src = 'video.webm'; // fallback.
+}
+  
 });
