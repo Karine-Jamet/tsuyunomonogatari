dc7c31833c9e5e18c5b8208069b621f80427ceef
Karine <karine.jamet203@gmail.com>
2016-04-22 16:13:43 +0200

    Ajout des animations de nuages

--- script.js
+++ script.js
@@ -55,6 +55,7 @@ $(document).ready(function() {
       $('.water').addClass('waterShow');
       $('.cloudLeft').addClass('cloudShow');
       $('.cloudRight').addClass('cloudShow');
+	  $('.cloudFond').addClass('cloudShow');
 
     }, 7000);
 
